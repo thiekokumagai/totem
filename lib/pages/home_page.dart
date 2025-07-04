80043bd9ffeb0352a48ab02204c39d6dd0f05fa8
Wesley Kumagai <wkumagai@inovvati.com.br>
2025-07-04 08:57:54 -0400

    feat: Componente rodape

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-
+import '../widgets/footer.dart'; 
 class HomePage extends StatelessWidget {
   final bool highlighted;
 
@@ -188,42 +188,7 @@ class HomePage extends StatelessWidget {
                 ),
               ),
 
-              // Separador do rodapé
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 30),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 15,
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      color: Color(0xFF032A28),
-                      borderRadius: BorderRadius.all(Radius.circular(999)),
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              // Rodapé fixo
-              Center(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Image.asset(
-                      'assets/images/inovvati-logo-rodape.png',
-                      height: 24,
-                    ),
-                    const SizedBox(width: 8),
-                    const Text(
-                      '© 2025 Inovati - Todos os direitos reservados',
-                      style: TextStyle(fontSize: 12),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 20),
+              const Footer(),
             ],
           ),
         ],
