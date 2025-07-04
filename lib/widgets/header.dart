c4b92c53346fc1c403b433981744078ef2430e79
Enzo Haruo França Okita <eokita@PSG.LOCAL>
2025-07-04 15:35:09 -0400

    feat: implementação da header

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -6,22 +6,86 @@ class Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
+    return Stack(
+      alignment: Alignment.center,
       children: [
-        // Texto centralizado
-        const Center(
-          child: Text(
-            'Seu Texto Aqui',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.black, // Altere conforme necessário
+        Positioned(
+          top: 0,
+          left: 0,
+          child: Image.asset(
+            'assets/images/vv.png',
+            width: 300,
+            height: 330,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Opacity(
+            opacity: 0.95,
+            child: Container(
+              width: 980,
+              height: 225,
+              margin: const EdgeInsets.only(top: 10),
+              alignment: Alignment.topCenter,
+              decoration: BoxDecoration(
+                gradient: const RadialGradient(
+                  colors: [Color(0xFF29A495), Color(0xFF54D5C6)],
+                  center: Alignment.center,
+                  radius: 1,
+                ),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Center(
+                child: Image.asset(
+                  'assets/images/logo-inovvati.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
             ),
           ),
         ),
-
-        const SizedBox(height: 20),
+        Positioned(
+          top: 280,
+          right: 100,
+          child: TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: const Color(0xFF032A28),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+              padding: EdgeInsets.zero,
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, '/encerrar');
+            },
+            child: SizedBox(
+              width: 170,
+              height: 70,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    'assets/images/logout_icon.svg',
+                    width: 30,
+                    height: 30,
+                    fit: BoxFit.contain,
+                  ),
+                  const SizedBox(width: 16),
+                  const Text(
+                    'Sair',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
